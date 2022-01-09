b46158737eb6fb409ecef1a072184c5f7ae4b85d
tbanel <thierrybanel@free.fr>
2022-01-09 18:27:35 +0100

    update copyleft

--- orgtbl-aggregate.el
+++ orgtbl-aggregate.el
@@ -1,6 +1,6 @@
 ;;; orgtbl-aggregate.el --- Create an aggregated Org table from another one  -*- coding:utf-8;-*-
 
-;; Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021  Thierry Banel
+;; Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  Thierry Banel
 
 ;; Authors:
 ;;   Thierry Banel tbanelwebmin at free dot fr
